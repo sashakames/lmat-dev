9182df41244913c75b68b3da38d7daf62a33a787
Sasha Ames <sasha@llnl.gov>
2014-05-01 08:25:03 -0700

    cleanup to address bad taxid bug

--- src/kmerdb/SortedDb.cpp
+++ src/kmerdb/SortedDb.cpp
@@ -116,7 +116,7 @@ void SortedDb<tid_T>::add_data(const char *filename, size_t stopper = 0, bool us
 
   static uint16_t count_marker = 0;
 
-
+  uint16_t two_count =  2;
 
   //  cout << "stopper set to: " << stopper << "\n";
 
@@ -383,9 +383,9 @@ void SortedDb<tid_T>::add_data(const char *filename, size_t stopper = 0, bool us
 	  
 	}	
 	
-	uint16_t tmp_count =  2;
 
-	mcpyinsdb(tmp_count, 2);
+
+	mcpyinsdb(two_count, 2);
 	m_cur_offset += 2;	
 
 
@@ -394,33 +394,19 @@ void SortedDb<tid_T>::add_data(const char *filename, size_t stopper = 0, bool us
 	if (p_br_map) {
 
 	  uint16_t tid_16 = (*p_br_map)[tid];
-	  if (!(tid_16 < p_br_map->size()+1)) {
+	  if (tid_16 > p_br_map->size()+1 || tid_16 == 0) {
 	    cout << "bad read: " << tid << " " << tid_16 << "\n";
 	    assert(0);
 	  }
 	  
-	  assert (tid_16 > 0);
 
 	  //	  cout << "adding-tid " ; 
 
 	  mcpyinsdb(tid_16, 2);
 	  m_cur_offset += 2;
 
-	  // reassign and add the human one
 
-	  tid = 9606;
-
-	  tid_16 = (*p_br_map)[tid];
-	  if (!(tid_16 < p_br_map->size()+1)) {
-	    cout << "bad read: " << tid << " " << tid_16 << "\n";
-	    assert(0);
-	  }
-	  
-	  assert (tid_16 > 0);
-
-	  //	  cout << "adding-tid " ; 
-
-	  mcpyinsdb(tid_16, 2);
+	  mcpyinsdb(HUMAN_16, 2);
 	  m_cur_offset += 2;
 
 
@@ -499,7 +485,7 @@ void SortedDb<tid_T>::add_data(const char *filename, size_t stopper = 0, bool us
 
 	//write the kmer
       //      memcpy(m_data[m_cur_page]+m_cur_offset, &kmer, 8);
-    if (taxid_q.size() > 0 && tmp_tid_count > 0)	{
+    if (taxid_q.size() > 1 && tmp_tid_count > 1)	{
 
 	// check for no reduction
 
@@ -528,7 +514,7 @@ void SortedDb<tid_T>::add_data(const char *filename, size_t stopper = 0, bool us
 	if (p_br_map) {
 
 	  uint16_t tid_16 = (*p_br_map)[tid];
-	  if (tid_16 == p_br_map->size()+1 || tid_16 == 0) {
+	  if (tid_16 > p_br_map->size()+1 || tid_16 == 0) {
 	    cout << "bad set: " << tid << " " << tid_16 << "\n";
 	    assert(0);
 	  }
@@ -617,12 +603,12 @@ void SortedDb<tid_T>::add_data(const char *filename, size_t stopper = 0, bool us
 
 
 	  uint16_t tid_16 = (*p_br_map)[tid];
-	  if (!(tid_16 < p_br_map->size()+1)) {
+	  if (tid_16 > p_br_map->size()+1 || tid_16 == 0) {
 	    cout << "bad read: " << tid << " " << tid_16 << "\n";
 	    assert(0);
 	  }
 	  
-	  assert (tid_16 > 0);
+
 
 	  //	  cout << "adding-tid " ; 
 
