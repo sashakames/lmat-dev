b177a662fcebd2c7cf3401d35d04cbbaa3a1db6c
Jonathan E. Allen <allen99@herd2.llnl.gov>
2014-08-15 16:04:17 -0700

    update release

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -120,7 +120,7 @@ done
 
 #copy nclass scripts
 
-for n in run_gl.sh run_cs.sh summary.py run_lmat.sh tolineage.py combine_fast.pl losummary_fast.pl losummary_fast_mc.sh pull_reads_mc.sh pull_reads.pl build_taxid_lst.sh build_taxid_lst.pl
+for n in run_gl.sh run_cs.sh summary.py run_lmat.sh tolineage.py combine_fast.pl gsummary_mc.sh gsummary.pl pull_reads_fq_mc.sh pull_reads_fq.pl losummary_fast.pl losummary_fast_mc.sh pull_reads_mc.sh pull_reads.pl build_taxid_lst.sh build_taxid_lst.pl
 do
     if [ ! -f ../nclass/$n ] ; then
 	echo $n not found
